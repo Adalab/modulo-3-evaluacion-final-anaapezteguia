781161e1539e50f525a61e3ddbbbab00635db731
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-03-04 10:22:47 +0100

    Run docs

--- docs/static/js/main.ad645c46.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-react-project"]=this["webpackJsonpmy-react-project"]||[]).push([[0],{16:function(e,t,c){},17:function(e,t,c){},24:function(e,t,c){"use strict";c.r(t);c(0);var n=c(9),r=c.n(n),o=c(11),s=(c(16),c(17),c(2)),i=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("main",{children:"Hola mundo"})})};r.a.render(Object(s.jsx)(o.a,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))}},[[24,1,2]]]);
-//# sourceMappingURL=main.ad645c46.chunk.js.map
